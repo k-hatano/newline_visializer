17c13f8ec9fa3d21cd2368c0d1fb8319ac495870
k-hatano <kent.ruffle.mgj626@gmail.com>
2022-10-20 04:53:48 +0900

    More fix about composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,14 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit0eb62f7954033603e114d119106704ea
 {
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->classMap = ComposerStaticInit0eb62f7954033603e114d119106704ea::$classMap;
 
         }, null, ClassLoader::class);
     }
